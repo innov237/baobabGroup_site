47239d47496c6e055fa674ad74bdf9494e748d36
innov237 <innov237@gmail.com>
2020-10-16 23:44:05 +0100

    responsive header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Header.css';
 import logo from '../../images/logo.png';
+import iconMenu from '../../images/menu.svg';
 
 const Header: React.FC = () =>
     (
         <div>
-            <nav className="navbar" role="navigation">
+            <nav className="navbar navbar-lg" role="navigation">
                 <div className="navbar navbar-expand">
                     <ul className="nav navbar-nav navbar-left">
                         <li className="nav-item"><a className="nav-link" href="#">Acceuil</a></li>
@@ -13,7 +14,7 @@ const Header: React.FC = () =>
                         <li className="nav-item"><a className="nav-link" href="#about">Portfolio</a></li>
                     </ul>
                 </div>
-                <div className="navbar navbar-expand">
+                <div className="navbar">
                     <a className="navbar-brand" href="#"><img src={logo} alt="logo" className="logo" /></a>
                 </div>
                 <div className="navbar navbar-expand">
@@ -22,6 +23,12 @@ const Header: React.FC = () =>
                         <li className="nav-item"><a className="nav-link" href="#about">A propos</a></li>
                     </ul>
                 </div>
+                <div className="navbar nav-menu">
+                    <ul className="nav navbar-nav navbar-left">
+                        <li className="nav-item"><a className="nav-link" href="#"><img src={iconMenu} className="menu-icon" style={{width:"50px"}} alt=""/></a></li>
+                    </ul>
+                </div>
+
             </nav>
         </div>
     );
